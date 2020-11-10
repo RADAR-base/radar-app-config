3eec6b390c89eee5163fc1b37a6386820be5036e
Joris Borgdorff <joris@thehyve.nl>
2020-11-10 16:52:01 +0100

    Simplify default runtime

--- radar-app-config/build.gradle.kts
+++ radar-app-config/build.gradle.kts
@@ -4,7 +4,17 @@ plugins {
 }
 
 application {
-    mainClassName = "org.radarbase.appconfig.MainKt"
+    mainClass.set("org.radarbase.appconfig.MainKt")
+    applicationDefaultJvmArgs = listOf(
+        "-Djava.security.egd=file:/dev/./urandom",
+        "--add-modules", "java.se",
+        "--add-exports", "java.base/jdk.internal.ref=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "--add-opens", "java.base/java.nio=ALL-UNNAMED",
+        "--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED",
+        "--add-opens", "java.management/sun.management=ALL-UNNAMED",
+        "--add-opens", "jdk.management/com.sun.management.internal=ALL-UNNAMED"
+    )
 }
 
 repositories {
