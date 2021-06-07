e49366acd8b768c8fc45fab2fc175946c3a4b1cb
Joris Borgdorff <joris@thehyve.nl>
2021-06-07 14:00:19 +0200

    Only show clients that are active

--- radar-app-config/src/main/kotlin/org/radarbase/appconfig/service/ClientService.kt
+++ radar-app-config/src/main/kotlin/org/radarbase/appconfig/service/ClientService.kt
@@ -5,17 +5,23 @@ import org.radarbase.jersey.util.CacheConfig
 import org.radarbase.jersey.util.CachedMap
 import java.time.Duration
 import jakarta.ws.rs.core.Context
+import org.radarbase.jersey.auth.AuthConfig
 import org.radarbase.management.client.MPClient
 import org.radarbase.management.client.MPOAuthClient
 
-class ClientService(@Context private val mpClient: MPClient) {
+class ClientService(
+    @Context private val mpClient: MPClient,
+    @Context private val authConfig: AuthConfig,
+) {
     private val clients = CachedMap(
         CacheConfig(
             refreshDuration = Duration.ofHours(1),
             retryDuration = Duration.ofMinutes(5)
         )
     ) {
-        mpClient.requestClients().map { it.id to it }.toMap(HashMap())
+        mpClient.requestClients()
+            .filter { authConfig.jwtResourceName in it.resourceIds }
+            .associateBy { it.id }
     }
 
     fun readClients(): Collection<MPOAuthClient> = clients.get().values
