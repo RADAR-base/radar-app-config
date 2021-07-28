5b6e19fdda1eab06ef65d4c42ea971b306d9553f
Joris Borgdorff <joris@thehyve.nl>
2021-07-28 17:11:17 +0200

    Reorganize ConditionRepository

--- radar-app-config/src/main/kotlin/org/radarbase/appconfig/persistence/ConditionRepository.kt
+++ radar-app-config/src/main/kotlin/org/radarbase/appconfig/persistence/ConditionRepository.kt
@@ -46,6 +46,23 @@ class ConditionRepository(
         getConditionOrNull(projectId, conditionName)
     }
 
+    fun deactivate(projectId: String, conditionName: String): ConditionEntity = transact {
+        getCondition(projectId, conditionName).apply {
+            status = EntityStatus.INACTIVE
+            deactivatedAt = Instant.now()
+            lastModifiedAt = Instant.now()
+        }.also { merge(it) }
+    }.uncached()
+
+    fun update(condition: ConditionEntity) = transact {
+        getCondition(condition.projectId, condition.name).apply {
+            title = condition.title
+            lastModifiedAt = Instant.now()
+            expression = condition.expression
+            rank = condition.rank
+        }.also { merge(it) }
+    }.cached()
+
     private fun EntityManager.getConditionOrNull(projectId: String, name: String): ConditionEntity? {
         val cache = hazelcastInstance.getMap<String, Long>(CONDITION_TOKEN)
         val cachedId = cache[conditionScopeString(name, projectId)]
@@ -72,23 +89,6 @@ class ConditionRepository(
 
     private fun EntityManager.getCondition(projectId: String, name: String): ConditionEntity = getConditionOrNull(projectId, name)
         ?: throw HttpNotFoundException("condition_not_found", "Condition $name is not part of project $projectId")
-
-    fun deactivate(projectId: String, conditionName: String): ConditionEntity = transact {
-        getCondition(projectId, conditionName).apply {
-            status = EntityStatus.INACTIVE
-            deactivatedAt = Instant.now()
-            lastModifiedAt = Instant.now()
-        }.also { merge(it) }
-    }.uncached()
-
-    fun update(condition: ConditionEntity) = transact {
-        getCondition(condition.projectId, condition.name).apply {
-            title = condition.title
-            lastModifiedAt = Instant.now()
-            expression = condition.expression
-            rank = condition.rank
-        }.also { merge(it) }
-    }.cached()
 
     private fun ConditionEntity.uncached(): ConditionEntity = apply {
         conditionCache -= conditionScopeString(name, projectId)
