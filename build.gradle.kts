f23878ad23b81682b4ae147080c45be37d7e28b1
Joris Borgdorff <joris@thehyve.nl>
2023-03-13 12:59:47 +0100

    Bump gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") apply false
