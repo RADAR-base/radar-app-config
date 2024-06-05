a0051a8fcbc930f36c8c6372ba27be6d8c3306f4
Bastiaan <bastiaan@thehyve.nl>
2024-06-05 17:46:29 +0200

    security updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,22 +2,22 @@ object Versions {
     const val project = "0.5.1"
 
     const val java = 17
-    const val kotlin = "1.9.10"
-    const val kotlinxSerialization: String = "1.5.1"
+    const val kotlin = "1.9.23"
+    const val kotlinxSerialization = "1.6.3"
 
-    const val radarCommons = "1.1.1"
+    const val radarCommons = "1.1.2"
     const val jackson = "2.15.2"
-    const val antlr = "4.13.0"
-    const val ktor: String = "2.3.2"
+    const val antlr = "4.13.1"
+    const val ktor = "2.3.11"
     const val coroutines = "1.7.2"
     const val radarJersey = "0.11.1"
     const val log4j2 = "2.20.0"
-    const val slf4j = "2.0.7"
-    const val hazelcast = "5.3.5"
+    const val slf4j = "2.0.13"
+    const val hazelcast = "5.4.0"
     const val hazelcastHibernate = "5.1.0"
     const val hazelcastKubernetes = "2.2.3"
 
     const val junit = "5.9.3"
-    const val hamcrest: String = "2.2"
-    const val mockitoKotlin: String = "5.0.0"
+    const val hamcrest = "2.2"
+    const val mockitoKotlin = "5.3.1"
 }
