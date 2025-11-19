16ed50651a7ce5178035699d9ac31920436c04e9
EIjo <eljo@live.nl>
2025-11-19 10:13:26 +0100

    feat: add versioning columns to config liquibase schema

--- radar-app-config/src/main/kotlin/org/radarbase/appconfig/persistence/entity/ConfigEntity.kt
+++ radar-app-config/src/main/kotlin/org/radarbase/appconfig/persistence/entity/ConfigEntity.kt
@@ -10,6 +10,7 @@ import jakarta.persistence.SequenceGenerator
 import jakarta.persistence.Table
 import org.hibernate.annotations.Cache
 import org.hibernate.annotations.CacheConcurrencyStrategy
+import java.time.Instant
 
 @Entity(name = "Config")
 @Table(name = "config")
@@ -34,4 +35,13 @@ class ConfigEntity {
     @Column
     @Lob
     var value: String? = null
+
+    @Column(name = "create_timestamp")
+    var createTimestamp: Instant? = null
+
+    @Column(name = "created_by_user")
+    var createdByUser: String? = null
+
+    @Column
+    var version: Int? = null
 }
