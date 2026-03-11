97163cf31dcc5b1563908726eef172fc2c6cbe0a
Pim van Nierop <pim@thehyve.nl>
2026-03-11 11:47:16 +0100

    Apply March 2026 security updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,21 @@ radarRootProject {
 project(":radar-expression-lang-antlr") {
     apply(plugin = "org.gradle.antlr")
 }
+
+allprojects {
+
+    // --- Vulnerability fixes start ---
+    dependencies {
+        plugins.withType<JavaPlugin> {
+            constraints {
+                add("implementation", rootProject.libs.jackson.bom) {
+                    because("Force safe version of Jackson across all modules")
+                }
+                add("implementation", rootProject.libs.jackson.core) {
+                    because("Force safe version of Jackson across all modules")
+                }
+            }
+        }
+    }
+    // --- Vulnerability fixes end ---
+}
